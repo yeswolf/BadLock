8e4349884a41fc7defd5724929d4a2b0999735d5
stanislav.dombrovsky <stanislav.dombrovsky@jetbrains.com>
2020-11-27 19:57:55 +0300

    Fix pseudotest

--- app/src/test/java/com/yeswolf/badlock/ApiTest.kt
+++ app/src/test/java/com/yeswolf/badlock/ApiTest.kt
@@ -4,6 +4,7 @@ import com.yeswolf.badlock.apkmirror.data.ApkMirrorRepository
 import com.yeswolf.badlock.apkmirror.data.ApkMirrorSource
 import com.yeswolf.badlock.packages.mappers.VersionMapper
 import com.yeswolf.badlock.plugins.data.PluginsListSource
+import com.yeswolf.badlock.schedulers.SchedulersProvider
 import org.junit.Test
 
 /**
@@ -16,7 +17,7 @@ class ApiTest {
     fun testPluginUrls() {
         //FIXME: of course, that's wrong, but we need to have at least something for now.
         val source = PluginsListSource()
-        val apkMirrorRepository = ApkMirrorRepository(ApkMirrorSource, VersionMapper)
+        val apkMirrorRepository = ApkMirrorRepository(ApkMirrorSource, VersionMapper, SchedulersProvider())
         source.plugins.forEach {
             it.versions = apkMirrorRepository.getPluginVersions(it)
             val url = ApkMirrorSource.latestVersionDirectDownloadURL(it)
